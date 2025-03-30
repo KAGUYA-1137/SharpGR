09ee117e4e58efc4fa3c5cebe0bae373f4833157
KAGUYA_1137 <checkran90@gmail.com>
2025-03-30 18:03:06 +0900

    ファイルを更新

--- SharpGR/Form1.Designer.cs
+++ SharpGR/Form1.Designer.cs
@@ -49,6 +49,9 @@
             timer1 = new System.Windows.Forms.Timer(components);
             trackBarDuration = new TrackBar();
             Timelabel = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
             ((System.ComponentModel.ISupportInitialize)trackBarVol).BeginInit();
             menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDownVol).BeginInit();
@@ -59,7 +62,8 @@
             // 
             // buttonPlay
             // 
-            buttonPlay.Location = new Point(12, 392);
+            buttonPlay.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonPlay.Location = new Point(12, 458);
             buttonPlay.Name = "buttonPlay";
             buttonPlay.Size = new Size(81, 45);
             buttonPlay.TabIndex = 0;
@@ -72,7 +76,7 @@
             trackBarVol.Location = new Point(61, 32);
             trackBarVol.Maximum = 100;
             trackBarVol.Name = "trackBarVol";
-            trackBarVol.Size = new Size(627, 45);
+            trackBarVol.Size = new Size(677, 45);
             trackBarVol.TabIndex = 1;
             trackBarVol.Scroll += trackBarVol_Scroll;
             // 
@@ -87,10 +91,11 @@
             // 
             // menuStrip1
             // 
+            menuStrip1.Dock = DockStyle.None;
             menuStrip1.Items.AddRange(new ToolStripItem[] { ヘルプToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(787, 24);
+            menuStrip1.Size = new Size(56, 24);
             menuStrip1.TabIndex = 4;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -117,7 +122,8 @@
             // 
             // numericUpDownVol
             // 
-            numericUpDownVol.Location = new Point(694, 37);
+            numericUpDownVol.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            numericUpDownVol.Location = new Point(744, 37);
             numericUpDownVol.Name = "numericUpDownVol";
             numericUpDownVol.Size = new Size(58, 23);
             numericUpDownVol.TabIndex = 5;
@@ -125,8 +131,9 @@
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label2.AutoSize = true;
-            label2.Location = new Point(758, 41);
+            label2.Location = new Point(808, 41);
             label2.Name = "label2";
             label2.Size = new Size(17, 15);
             label2.TabIndex = 2;
@@ -135,9 +142,9 @@
             // statusStrip1
             // 
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
-            statusStrip1.Location = new Point(0, 440);
+            statusStrip1.Location = new Point(0, 506);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(787, 22);
+            statusStrip1.Size = new Size(837, 22);
             statusStrip1.TabIndex = 6;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -145,59 +152,67 @@
             // 
             toolStripStatusLabel1.Font = new Font("ＭＳ Ｐゴシック", 9F, FontStyle.Bold, GraphicsUnit.Point, 128);
             toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(772, 17);
+            toolStripStatusLabel1.Size = new Size(822, 17);
             toolStripStatusLabel1.Spring = true;
             // 
             // Namelabel
             // 
             Namelabel.AutoSize = true;
-            Namelabel.Font = new Font("Yu Gothic UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 128);
-            Namelabel.Location = new Point(17, 88);
+            Namelabel.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            Namelabel.Location = new Point(325, 113);
             Namelabel.Name = "Namelabel";
-            Namelabel.Size = new Size(0, 17);
+            Namelabel.Size = new Size(33, 20);
             Namelabel.TabIndex = 7;
+            Namelabel.Text = "----";
             // 
             // Artistlabel
             // 
             Artistlabel.AutoSize = true;
-            Artistlabel.Font = new Font("Yu Gothic UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            Artistlabel.Location = new Point(17, 115);
+            Artistlabel.Font = new Font("Yu Gothic UI", 11.25F);
+            Artistlabel.Location = new Point(325, 142);
             Artistlabel.Name = "Artistlabel";
-            Artistlabel.Size = new Size(0, 17);
+            Artistlabel.Size = new Size(33, 20);
             Artistlabel.TabIndex = 7;
+            Artistlabel.Text = "----";
             // 
             // Albumlabel
             // 
             Albumlabel.AutoSize = true;
-            Albumlabel.Font = new Font("Yu Gothic UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            Albumlabel.Location = new Point(17, 139);
+            Albumlabel.Font = new Font("Yu Gothic UI", 11.25F);
+            Albumlabel.Location = new Point(325, 171);
             Albumlabel.Name = "Albumlabel";
-            Albumlabel.Size = new Size(0, 17);
+            Albumlabel.Size = new Size(33, 20);
             Albumlabel.TabIndex = 7;
+            Albumlabel.Text = "----";
             // 
             // Yearlabel
             // 
             Yearlabel.AutoSize = true;
-            Yearlabel.Font = new Font("Yu Gothic UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            Yearlabel.Location = new Point(17, 163);
+            Yearlabel.Font = new Font("Yu Gothic UI", 11.25F);
+            Yearlabel.Location = new Point(297, 220);
             Yearlabel.Name = "Yearlabel";
-            Yearlabel.Size = new Size(0, 17);
+            Yearlabel.Size = new Size(37, 20);
             Yearlabel.TabIndex = 7;
+            Yearlabel.Text = "20--";
+            Yearlabel.Visible = false;
             // 
             // Circlelabel
             // 
             Circlelabel.AutoSize = true;
-            Circlelabel.Font = new Font("Yu Gothic UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            Circlelabel.Location = new Point(17, 187);
+            Circlelabel.Font = new Font("Yu Gothic UI", 11.25F);
+            Circlelabel.Location = new Point(297, 200);
             Circlelabel.Name = "Circlelabel";
-            Circlelabel.Size = new Size(0, 17);
+            Circlelabel.Size = new Size(33, 20);
             Circlelabel.TabIndex = 7;
+            Circlelabel.Text = "----";
+            Circlelabel.Visible = false;
             // 
             // AlbumArtpictureBox
             // 
-            AlbumArtpictureBox.Location = new Point(413, 83);
+            AlbumArtpictureBox.Anchor = AnchorStyles.Right;
+            AlbumArtpictureBox.Location = new Point(17, 113);
             AlbumArtpictureBox.Name = "AlbumArtpictureBox";
-            AlbumArtpictureBox.Size = new Size(362, 291);
+            AlbumArtpictureBox.Size = new Size(256, 252);
             AlbumArtpictureBox.SizeMode = PictureBoxSizeMode.Zoom;
             AlbumArtpictureBox.TabIndex = 8;
             AlbumArtpictureBox.TabStop = false;
@@ -210,33 +225,69 @@
             // 
             // trackBarDuration
             // 
+            trackBarDuration.Anchor = AnchorStyles.Bottom;
             trackBarDuration.Enabled = false;
-            trackBarDuration.Location = new Point(99, 392);
+            trackBarDuration.Location = new Point(99, 458);
             trackBarDuration.Name = "trackBarDuration";
-            trackBarDuration.Size = new Size(676, 45);
+            trackBarDuration.Size = new Size(726, 45);
             trackBarDuration.TabIndex = 9;
             // 
             // Timelabel
             // 
+            Timelabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             Timelabel.AutoSize = true;
             Timelabel.Font = new Font("Yu Gothic UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            Timelabel.Location = new Point(17, 359);
+            Timelabel.Location = new Point(17, 428);
             Timelabel.Name = "Timelabel";
-            Timelabel.Size = new Size(0, 17);
+            Timelabel.Size = new Size(67, 17);
             Timelabel.TabIndex = 7;
+            Timelabel.Text = "--:-- / --:--";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            label3.Location = new Point(297, 113);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 20);
+            label3.TabIndex = 7;
+            label3.Text = "♪";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Yu Gothic UI", 11.25F);
+            label4.Location = new Point(297, 142);
+            label4.Name = "label4";
+            label4.Size = new Size(26, 20);
+            label4.TabIndex = 7;
+            label4.Text = "👤";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Yu Gothic UI", 11.25F);
+            label5.Location = new Point(297, 171);
+            label5.Name = "label5";
+            label5.Size = new Size(29, 20);
+            label5.TabIndex = 7;
+            label5.Text = "📀";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(787, 462);
+            ClientSize = new Size(837, 528);
             Controls.Add(trackBarDuration);
             Controls.Add(AlbumArtpictureBox);
             Controls.Add(Timelabel);
             Controls.Add(Circlelabel);
             Controls.Add(Yearlabel);
+            Controls.Add(label5);
             Controls.Add(Albumlabel);
+            Controls.Add(label4);
             Controls.Add(Artistlabel);
+            Controls.Add(label3);
             Controls.Add(Namelabel);
             Controls.Add(statusStrip1);
             Controls.Add(numericUpDownVol);
@@ -289,5 +340,8 @@
         private System.Windows.Forms.Timer timer1;
         public TrackBar trackBarDuration;
         public Label Timelabel;
+        public Label label3;
+        public Label label4;
+        public Label label5;
     }
 }
