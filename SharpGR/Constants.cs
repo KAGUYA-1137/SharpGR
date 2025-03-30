cf348502a4ab79a1f10dc4785729ac56a6c7aa11
KAGUYA_1137 <checkran90@gmail.com>
2025-03-30 18:01:46 +0900

    ファイルを更新

--- SharpGR/Constants.cs
+++ SharpGR/Constants.cs
@@ -14,5 +14,20 @@
         /// メイン画面の設定ファイル名
         /// </summary>
         public const string FORM_MAIN_SETTING_FILE_NAME = $"{SETTINGS_FOOTER}Form1.json";
+
+        /// <summary>
+        /// 幻想郷ラジオのストリームエンドポイント
+        /// </summary>
+        public const string STREAM_ENDPOINT = "https://stream.gensokyoradio.net/3";
+
+        /// <summary>
+        /// 幻想郷ラジオで再生中の楽曲情報の取得先
+        /// </summary>
+        public const string MUSIC_INFO_JSON = "https://gensokyoradio.net/api/station/playing/";
+
+        /// <summary>
+        /// 幻想郷ラジオのアルバムアート取得先のプレフィックス
+        /// </summary>
+        public const string ALBUM_ART_PREFIX = "https://gensokyoradio.net/images/albums/500/";
     }
 }
